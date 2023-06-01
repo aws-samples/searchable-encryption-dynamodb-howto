7c7ce94b35d75d108c6497f08e101e20d15a7ec6
seebees <ryanemer@amazon.com>
2023-05-31 21:25:05 -0700

    hack

--- exercises/java/exercise-1/src/main/java/sfw/example/dbesdkworkshop/CreateBranchKey.java
+++ exercises/java/exercise-1/src/main/java/sfw/example/dbesdkworkshop/CreateBranchKey.java
@@ -10,6 +10,9 @@ import picocli.CommandLine.ParameterException;
 import picocli.CommandLine.Spec;
 import sfw.example.dbesdkworkshop.AwsSupport;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 @Command(name = "create-branch-key", description = "Creates a branch key.")
 public class CreateBranchKey implements Runnable {
 
@@ -18,8 +21,24 @@ public class CreateBranchKey implements Runnable {
 
   @Override
   public void run() {
-    String keyId = AwsSupport.CreateBranchKey(shared.ddbLocal);
 
-    System.out.println("Created branch key : " + keyId);
+    // Given the order of the steps in the workshop
+    // This needs to be able to compile _before_ it can work.
+    // This code is conveyance to make the steps flow
+    // it is not intended as a good example of how to call a function.
+
+    try {
+      final Method method = AwsSupport.class.getDeclaredMethod("CreateBranchKey");
+      final Object result = method.invoke(null, shared.ddbLocal);
+      final String keyId = (String) result;
+      System.out.println("Created branch key : " + keyId);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    } catch (InvocationTargetException e) {
+      throw new RuntimeException(e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+
   }
 }
